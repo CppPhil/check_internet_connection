ffc4e9dd40dd44e393817641f25e2eda5ed45ba4
CppPhil <CppPhil@users.noreply.github.com>
2020-04-12 15:53:27 +0200

    Add format.sh

---
+++ format.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+### This script is used to run clang-format on the source files.
+
+# Directory containing this bash script.
+readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+readonly PREV_DIR=$(pwd)
+
+readonly DOT_CLANG_FORMAT=$DIR/.clang-format
+
+delete_file_if_exists () {
+  if [ -e $1 ]; then
+    rm -f $1
+  fi
+}
+
+format_cmake () {
+  cmakelists_file=$1
+  tmp_file="${cmakelists_file}.tmp"
+
+  cmake-format $cmakelists_file > $tmp_file
+  rm -f $cmakelists_file
+  mv $tmp_file $cmakelists_file
+}
+
+format () {
+  cd $1
+  delete_file_if_exists ./.clang-format
+  cp $DOT_CLANG_FORMAT ./.clang-format
+  find -name '*.cpp' -o -name '*.hh' -o -name '*.hpp' -o -name '*.h' | xargs clang-format -i
+  rm -f ./.clang-format
+
+  format_cmake $(find -name 'CMakeLists.txt')
+}
+
+cd $DIR
+
+format_cmake ./CMakeLists.txt
+
+# Format the source files.
+format include
+format src
+
+cd $PREV_DIR
+
+exit 0
+
